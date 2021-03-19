dc39fd9f3214bd89d893cc91540e87b81e44fd98
quantosh <80041279+quantosh@users.noreply.github.com>
2021-03-19 17:31:21 +0100

    refactor

--- Section Five/BreakAndContinue.java
+++ Section Five/BreakAndContinue.java
@@ -1,5 +1,13 @@
 public class BreakAndContinue {
     public static void main(String[] args) {
-        
+        // continue skips a run, and continues to the next one.
+        for (int i = 0; i<=10; i++){
+            if(i % 2 != 0){
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        // break tje loop 
     }    
 }
