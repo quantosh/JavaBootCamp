45fdbcd3898322bc6e1099c5bd9b53f3b920c94e
quantosh <80041279+quantosh@users.noreply.github.com>
2021-03-19 16:45:15 +0100

    Dice Roll Doubles exersice

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Workbook 5.10/Doubles.java
@@ -1,36 +1,36 @@
 public class Doubles {
     public static void main(String[] args) {
 
-        /* Task 2
-            1. Call the rollDice() function twice.
-            2. Store the return values in dice1 and dice2.   
+        int dice1 = rollDice();
+        int dice2 = rollDice();
 
-        */
-        int dice1;
-        int dice2;
+        System.out.println("\nProgram stop if dice numbers are the same!");
+        System.out.println("Dice 1: " + dice1);
+        System.out.println("Dice 2: " + dice2 + "\n");
 
-   //      System.out.println("Dice 1: " + dice1);
-   //      System.out.println("Dice 2: " + dice2 + "\n");
-
-        /* Task 3
-            1. Set up a loop that keeps running while the two dice aren't the same.
-            2. During each run, re-roll the dice and print the two values. 
-         */
-
-
-        // Task 4: After they roll doubles, print: You rolled doubles!
+        // 1. Set up a loop that keeps running while the two dice aren't the same.
+        // 2. During each run, re-roll the dice and print the two values.
+        boolean same = true;
+        while (same) {
+            if (dice1 == dice2) {
+                System.out.println("You rolled doubles: ");
+                System.out.println("Dice 1: " + dice1);
+                System.out.println("Dice 2: " + dice2 + "\n");
+                same = false;
+            } else {
+                dice1 = rollDice();
+                dice2 = rollDice();
+                System.out.println("Dice 1: " + dice1);
+                System.out.println("Dice 2: " + dice2 + "\n");
+            }
+        }
     }
 
-
-    /** Task 1 
-     * Function name: rollDice – rolls a dice between 1 and 6
-     * @return randomNumber (int)
-     * 
-     */
+    public static int rollDice() {
+        double random = Math.random() * 6;
+        int rToInt = (int) random;
+        rToInt += 1;
+        return rToInt;
+    }
 
 }
-
-
-
-
-
