41ab440396efc029d30d89fdc277794a57aff0da
quantosh <80041279+quantosh@users.noreply.github.com>
2021-03-23 13:54:32 +0100

    Blackjack 0.3

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/5. Loops/Challenge/Blackjack.java
@@ -9,71 +9,253 @@ public class Blackjack {
         System.out.println("Do you have a knack for Black Jack?");
         System.out.println("We shall see..");
         System.out.println("..Ready? Press anything to begin!");
-        //Task 3 – Wait for the user to press enter.
-        //Task 4 – Get two random cards.
-        //       – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
+        // Task 3 – Wait for the user to press enter.
+        scan.nextLine();
+        // Task 4 – Get two random cards.
+        int user1 = drawRandomCard();
+        int user2 = drawRandomCard();
+        final int userPoints = user1 + user2;
+        String userCard1 = cardString(user1);
+        String userCard2 = cardString(user2);
 
-        //Task 5 – Print the sum of your hand value.
-        //       – print: your total is: <hand value>
+        // – Print them: \n You get a \n" + <randomCard> + "\n and a \n" + <randomCard>
+        System.out.println("You get a: \n" + userCard1);
+        System.out.println("And a: \n" + userCard2);
+        // Task 5 – Print the sum of your hand value.
+        // – print: your total is: <hand value>
+        System.out.println("The points of your cards are: " + userPoints);
 
-        
-        //Task 6 – Get two random cards for the dealer.
-        //       – Print: The dealer shows \n" + <first card> + "\nand has a card facing down \n" + <facedown card>
-        //       – Print: \nThe dealer's total is hidden
+        // Task 6 – Get two random cards for the dealer.
+        // – Print: The dealer shows \n" + <first card> + "\nand has a card facing down
+        // \n" + <facedown card>
+        // – Print: \nThe dealer's total is hidden
+        int cpu1 = drawRandomCard();
+        int cpu2 = drawRandomCard();
+        int cpuPoints = cpu1 + cpu2;
+        String dealerCard1 = cardString(cpu1);
+        String dealerCard2 = cardString(cpu2);
 
-       
-        //Task 8 – Keep asking the player to hit or stay (while loop).
-        //       1. Every time the player hits
-        //             – draw a new card.
-        //             – calculate their new total.
-        //             – print: (new line) You get a (new line) <show new card>.
-        //             - print: your new total is <total>
+        System.out.println("Dealer get a: \n" + dealerCard1);
+        System.out.println("Has a facing card down: \n" + faceDown());
+        // Task 8 – Keep asking the player to hit or stay (while loop).
+        // 1. Every time the player hits
+        // – draw a new card.
+        // – calculate their new total.
+        // – print: (new line) You get a (new line) <show new card>.
+        // - print: your new total is <total>
 
-        //       2. Once the player stays, break the loop. 
+        // 2. Once the player stays, break the loop.
+        String hitOrStay = hitOrStay();
+        if (hitOrStay.equals("hit")) {
+            int userHit = drawRandomCard();
+            System.out.println("You get a: \n" + cardString(userHit));
+            int total = userPoints + userHit; // Sumo los pts de la carta a pts totales
+            System.out.println("Your new total is: " + total);
+        }
+        // For tasks 9 to 13, see the article: Blackjack Part II.
+        if (total > 21){
+            System.out.println("Bust! Player loses");
+            System.exit(0);
+        }
+        System.out.println("Dealers Turn");
+        System.out.println("Dealers Turn");
+        System.out.println("Dealers Turn");
 
-        
-        //For tasks 9 to 13, see the article: Blackjack Part II. 
-         scan.close();
+        scan.close();
 
     }
 
-    /** Task 1 – make a function that returns a random number between 1 and 13
+    /**
+     * Task 1 – make a function that returns a random number between 1 and 13
      * Function name – drawRandomCard
+     * 
      * @return (int)
      *
-     * Inside the function:
-     *   1. Gets a random number between 1 and 13.
-     *   2. Returns a card.
+     *         Inside the function: 1. Gets a random number between 1 and 13. 2.
+     *         Returns a card.
      */
+    public static int drawRandomCard() {
+        double random = Math.random() * 13;
+        int rToInt = (int) random;
+        rToInt += 1;
+        return rToInt;
+    }
 
-    /** Task 2 – make a function that returns a String drawing of the card.
-     * Function name – cardString
+    /**
+     * Task 2 – make a function that returns a String drawing of the card. Function
+     * name – cardString
+     * 
      * @param cardNumber (int)
      * @return (String)
      *
-     * Inside the function:
-     *   1. Returns a String drawing of the card.
+     *         Inside the function: 1. Returns a String drawing of the card.
      */
+
+    public static String cardString(int cardNumber) {
+        String card;
+        switch (cardNumber) {
+        case 1:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 2:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u25022\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25912\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 3:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u25023\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25913\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 4:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u25024\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25914\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 5:
+            card = " \u25025\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25915\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 6:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u25026\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25916\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 7:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u25027\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25917\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 8:
+            card = " \u25028\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2591\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25918\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 9:
+            card = " \u25029\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u25919\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 10:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u250210\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2660\u2591\u2660\u2591\u2660\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2660\u2591\u259110\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 11:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u2502J\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2588\u2588\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2588\u2591\u2591\u2588\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2588\u2588\u2588\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591J\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 12:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u2502Q\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2588\u2588\u2588\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2588\u2591\u2591\u2591\u2588\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2588\u2588\u2588\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2588\u2591Q\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        case 13:
+            card = " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                    + " \u2502K\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2588\u2588\u2591\u2591\u2588\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2588\u2588\u2588\u2588\u2591\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2588\u2588\u2591\u2591\u2588\u2591\u2591\u2502 \n"
+                    + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591K\u2502 \n"
+                    + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
+            break;
+        default:
+            card = "el pepe";
+        }
+        return card;
+    }
 
     public static String faceDown() {
-        return
-        "   _____\n"+
-        "  |     |\n"+ 
-        "  |  J  |\n"+
-        "  | JJJ |\n"+
-        "  |  J  |\n"+
-        "  |_____|\n";
-    }
-    
-    /** Task 7 – make a function that asks the user to hit or stay.
-     * Function name – hitOrStay
-     * @return (String)
-     *
-     * Inside the function:
-     *   1. Asks the user to hit or stay.
-     *   2. If the user doesn't enter "hit" or "stay", keep asking them to try again by printing:
-     *      Please write 'hit' or 'stay'
-     *   3. Returns the user's option 
-     */
+        return " \u250C\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2510 \n"
+                + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                + " \u2502\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2591\u2502 \n"
+                + " \u2514\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2500\u2518 \n";
     }
 
+    /**
+     * Task 7 – make a function that asks the user to hit or stay. Function name –
+     * hitOrStay
+     * 
+     * @return (String)
+     *
+     *         Inside the function: 1. Asks the user to hit or stay. 2. If the user
+     *         doesn't enter "hit" or "stay", keep asking them to try again by
+     *         printing: Please write 'hit' or 'stay' 3. Returns the user's option
+     */
+    public static String hitOrStay() {
+        System.out.println("You hit or stay? Please write 'hit' or ''stay'");
+        String option = scan.nextLine();
+        boolean stayOrHit = true;
+        while(stayOrHit){
+            if (option.equals("hit")) {
+                return "hit";
+            } else if (option.equals("stay")) {
+                stayOrHit = false;
+                return "stay";
+            } else if (!option.equals("stay") || !option.equals("hit")) {
+                boolean equal = true;
+                while (equal) {
+                    System.out.println("Please write 'hit' or 'stay'.");
+                    option = scan.nextLine();
+                    if (option.equals("stay") || !option.equals("hit")) {
+                        equal = false;
+                    }
+                }
+            }
+        }
+        return option;
+    }
+
+}
