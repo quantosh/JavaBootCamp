ecbe94dbcc62decccd2fa50007d77fd836319e43
quantosh <80041279+quantosh@users.noreply.github.com>
2021-03-25 13:57:41 +0100

    java

--- Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
+++ Java-Bootcamp-Resources-main/Module 1 - Introduction to Java/6. Arrays/Workbook 6.5/HighScore.java
@@ -8,14 +8,21 @@ public class HighScore {
          *         Example – Here are the scores: 14775 48328 7928 27102 21787 21063 38096 42711 32863 23707
          */
         int highScore = 0;
-        int
+        int[] score = {};
          /** Task 3
           *  
           *   1. Using a for loop, find the highest score in the array and update the highScore.
           *   2. println("\n\nThe highest score is: <>. Give that man a cookie!");
 
           */
-
+        
+        for(int i = 0; i < 9; i++){
+            score[i] = randomNumber();
+            if (score[i] >= highScore){
+                highScore = score[i];
+                System.out.println("\n\nThe highest score is: " +score[i]+ ". Give that man a cookie!");
+            }            
+        }
 
     }
     /** Task 1
@@ -25,6 +32,12 @@ public class HighScore {
      * Inside the function
      *  - returns a random number between 0 and 50000
      */
-    
+    public static int randomNumber(){
+        double random = Math.random() * 50000;
+        int rToInt = (int)random;
+        rToInt += 1;
+        return rToInt;
+    }
+
 
 }
