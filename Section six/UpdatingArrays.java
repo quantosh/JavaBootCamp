38faa6bf74a6debf9933cc825e47a1275ca92e48
quantosh <80041279+quantosh@users.noreply.github.com>
2021-03-26 11:31:50 +0100

    Updating Arrays

--- Section six/UpdatingArrays.java
+++ Section six/UpdatingArrays.java
@@ -5,8 +5,15 @@ public class UpdatingArrays {
         String[] menu = {"Espresso", "Iced Coffee", "Macchiato"};
         menu[2] = "Latte";
         String menuString = Arrays.toString(menu);
+
+        // Creating new array that can hold 5 
         String[] newMenu = new String[5];
 
-
+        for (int i = 0; i < menu.length; i++){
+            newMenu[i] = menu[i];
+        }
+        newMenu[3] = "House Blend";
+        newMenu[4] = "Dark Roast";
+        System.out.println(Arrays.toString(newMenu));
     }
 }
