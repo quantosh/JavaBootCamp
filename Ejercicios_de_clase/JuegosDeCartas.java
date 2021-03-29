642e4608d526d38d6311235632ac918d797e8564
quantosh <80041279+quantosh@users.noreply.github.com>
2021-03-29 13:37:01 +0200

    Ejercicio juegos de cartas

---
+++ Ejercicios_de_clase/JuegosDeCartas.java
@@ -0,0 +1,20 @@
+public class JuegosDeCartas {
+    public static void main(String[] args) {
+        
+    }
+
+    /*
+    Creación del paquete org.ieszadinvergeles.juegosdecartas que contendrá los siguientes elementos:
+    Clase Carta.
+        Enumeración PaloNaipe.
+        Enumeración RangoNaipe.
+        Clase Baraja.
+    */
+
+    /*
+    Implementación del juego del BlackJack. Se creará el paquete org.ieszaidinvergeles.juegosdecartas.blackjack que contendrá los siguientes elementos.
+        Clase Jugador.
+        Clase Banca.
+        Clase BlackJack. Contiene el método main del programa principal.
+    */
+}
